eec3d3a6377a0cbf69e0ed94123cee9ab13edb8b
chandrahasd <dewangan.chandrahas@gmail.com>
2019-03-02 02:55:21 +0530

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='ExperimentManager',
+      version='1.0',
+      description='Experiment Manager Utility',
+      author='Chandrahas',
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      author_email='chandrahas@iisc.ac.in',
+      packages=['em'],
+      long_description=open('README.md').read(),
+     )
